7df225c964cf1c5746ebc60148e87c399498d438
zcchen <chen-bill-bill@gmail.com>
2020-03-21 17:31:28 +0800

    added windows autoload support by git submodule

--- make.bat
+++ make.bat
@@ -1,34 +1,45 @@
-@ECHO ON
+@ECHO OFF
 
 REM Since it is difficult to use `make` in windows,
 REM using `make.bat` is the easiest way to setup my env.
 
-SET VIMRC_SRC="_.vimrc"
-SET VIMDIR_SRC="_.vim"
-SET CVIMRC_SRC="_.cvimrc"
+SET VIMRC_SRC=_.vimrc
+SET VIMDIR_SRC=_.vim
+SET CVIMRC_SRC=_.cvimrc
 
-SET REMOVE_CMD="del"
-SET LINK_CMD="mklink"
-SET MKDIR_CMD="mkdir"
-SET PWD_CMD="cd"
-SET WGET_CMD="certutil.exe -urlcache -split -f"
-SET WGET_OUTPUT=""
-SET VIMRC_OBJ_WIN="%HOME%/_vimrc"
-SET VIMRC_OBJ_UNIX="%HOME%/.vimrc"
-SET VIMDIR_OBJ_WIN="%HOME%/vimfiles"
-SET VIMDIR_OBJ_UNIX="%HOME%/vimfiles"
-REM SET CVIMRC_SRC="%HOME%/_cvimrc"
+SET WGET_CMD=certutil.exe -urlcache -split -f
+SET WGET_OUTPUT=
+SET VIMRC_OBJ_WIN=%UserProfile%\_vimrc
+SET VIMRC_OBJ_UNIX=%UserProfile%\.vimrc
+SET VIMDIR_OBJ_WIN=%UserProfile%\vimfiles
+SET VIMDIR_OBJ_UNIX=%UserProfile%\.vim
+REM SET CVIMRC_SRC=%UserProfile%\_cvimrc
 
-SET PLUGIN_MNG_URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-SET PLUGIN_MNG_NAME="plug.vim"
+REM SET PLUGIN_MNG_URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+REM SET PLUGIN_MNG_NAME=plug.vim
 
+ECHO "=================="
+ECHO "grant this permission to set Local Policies\User Rights Assignment\Create symbolic links"
+ECHO "=================="
+start secpol.msc
+PAUSE
 
-%LINK_CMD% /J %VIMRC_OBJ_WIN%   "%PWD_CMD%\\%VIMRC_SRC%"
-%LINK_CMD% /J %VIMDIR_OBJ_WIN%  "%PWD_CMD%\\%VIMDIR_SRC%"
-%LINK_CMD% %VIMRC_OBJ_UNIX%     %VIMRC_OBJ_WIN%
-%LINK_CMD% %VIMDIR_OBJ_UNIX%    %VIMDIR_OBJ_WIN%
-%MKDIR_CMD% "%VIMDIR_OBJ_WIN%\\autoload"
-%WGET_CMD% "%PLUGIN_MNG_URL%" %WGET_OUTPUT% "%VIMDIR_OBJ_WIN%/autoload/%PLUGIN_MNG_NAME%"
+RD /Q "%VIMRC_OBJ_WIN%"
+RD /S /Q "%VIMDIR_OBJ_WIN%"
+RD /Q "%VIMRC_OBJ_UNIX%"
+RD /S /Q "%VIMDIR_OBJ_UNIX%"
+MKLINK     "%VIMRC_OBJ_WIN%"   %CD%\%VIMRC_SRC%
+MKLINK /J "%VIMDIR_OBJ_WIN%"  %CD%\%VIMDIR_SRC%
+MKLINK     "%VIMRC_OBJ_UNIX%"     "%VIMRC_OBJ_WIN%"
+MKLINK /J "%VIMDIR_OBJ_UNIX%"    "%VIMDIR_OBJ_WIN%"
+REM MKDIR "%VIMDIR_OBJ_WIN%\autoload"
+REM %WGET_CMD% %PLUGIN_MNG_URL% %WGET_OUTPUT% "%VIMDIR_OBJ_WIN%\autoload\%PLUGIN_MNG_NAME%"
+
+ECHO "=================="
+ECHO "Remove this permission to set Local Policies\User Rights Assignment\Create symbolic links"
+ECHO "=================="
+start secpol.msc
+PAUSE
 
 ECHO "Windows link is finished..."
 PAUSE
