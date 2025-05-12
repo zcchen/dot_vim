8b8d6c09c951a991f104fc89a021164af7ca3ff8
zcchen <Bill.Chen.fs.cn@outlook.com>
2025-05-12 16:25:01 +0800

    Added rg.nvim with cmd: Rg, Rgp, Rgf, Rgfp, which is similar as ack.

--- nvim/lua/lazy-plugins/files.lua
+++ nvim/lua/lazy-plugins/files.lua
@@ -119,6 +119,24 @@ return {
         end,
     },
     {
+        "doums/rg.nvim",
+        opts = {
+            -- Optional function to be used to format the items in the
+            -- quickfix window (:h 'quickfixtextfunc')
+            qf_format = nil,
+            -- Glob list of excluded files and directories when the special
+            -- `e` flag is set (it uses the `--glob !*` flag of rg)
+            excluded = {
+                '.idea',
+                'node_modules',
+                '.git',
+                'target',
+                'package-lock.json',
+                'Cargo.lock',
+            },
+        },
+    },
+    {
         'nvim-telescope/telescope.nvim',
         dependencies = {
             'nvim-lua/plenary.nvim'
